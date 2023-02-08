8ecc4cd3566a1bc1795a79a448da014faba5c549
Gia Bao <edwardgbao@gmail.com>
2023-02-08 08:10:32 +0700

    add test

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React2
+          Learn React1
         </a>
       </header>
     </div>
