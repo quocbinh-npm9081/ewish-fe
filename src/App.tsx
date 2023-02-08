1cfa0d656b66c90a6e84be57754374f25e357529
Gia Bao <edwardgbao@gmail.com>
2023-02-08 08:12:13 +0700

    bao da thay doi

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React1
+          Learn React1Zzz
         </a>
       </header>
     </div>
